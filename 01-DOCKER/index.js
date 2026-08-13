42e763b7590e291c7c95118c4d3ba39f53b1075b
saishshaikh <saishshaikh2323@gmail.com>
2026-08-13 20:23:00 +0530

    Docker

--- 01-DOCKER/index.js
+++ 01-DOCKER/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.get("/",(req,res)=>{
-   return res.status(200).json({message : "Hello from docker"})
+   return res.status(200).json({message : "Hello  Saish from docker"})
 })
 
 
